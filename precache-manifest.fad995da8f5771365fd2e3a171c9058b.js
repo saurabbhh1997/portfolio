35d03442a98930c51388bba980b95105d2a83d18
saurabh <97597058+saurabbhh1997@users.noreply.github.com>
2022-11-08 21:47:44 +0530

    Add files via upload

---
+++ precache-manifest.fad995da8f5771365fd2e3a171c9058b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "5812fff7a41fe176e04a",
+    "url": "/portfolio/static/css/main.894e1707.chunk.css"
+  },
+  {
+    "revision": "5812fff7a41fe176e04a",
+    "url": "/portfolio/static/js/main.5812fff7.chunk.js"
+  },
+  {
+    "revision": "959d45317b523a896205",
+    "url": "/portfolio/static/css/1.af0c78cb.chunk.css"
+  },
+  {
+    "revision": "959d45317b523a896205",
+    "url": "/portfolio/static/js/1.959d4531.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "0a8cf10ed941810c0051a81e9a1572eb",
+    "url": "/portfolio/static/media/me.0a8cf10e.png"
+  },
+  {
+    "revision": "7d1fe24b3072b92a2bd11f8a5d569f9b",
+    "url": "/portfolio/static/media/me-about.7d1fe24b.jpg"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/portfolio/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/portfolio/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/portfolio/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/portfolio/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/portfolio/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/portfolio/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/portfolio/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "b77746cf34b9dd52a559420d6399bd7c",
+    "url": "/portfolio/index.html"
+  }
+];
